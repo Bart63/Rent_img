f9f3867d2e6a637e15de1414ee505154d0b978e6
lewyy34 <229939@edu.p.lodz.pl>
2021-05-03 23:32:44 +0200

    Commit

--- Validator.py
+++ Validator.py
@@ -1,5 +1,7 @@
+import os.path
+
 class Validator:
-    def IsString(self) -> bool:
+    def IsString(self):
         pass
 
     def IsQuery(self) -> bool:
@@ -7,3 +9,18 @@ class Validator:
 
     def IsBalance(self) -> bool:
         pass
+
+    def IsValidString(self,text,max_length) -> bool:
+        if (type(text)==str and len(text)<=max_length and len(text) > 0):
+            return True
+        else:
+            return False
+
+    def IsValidInt(self,value,max_val) -> bool:
+            if(type(value)==int and value<=max_val and value>0 ):
+                return True
+            else:
+                return False
+
+    def IsValidPath(self,path,):
+
