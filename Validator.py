75410ee157290a2bc0734178ef04b387cf6a2908
Bart63 <bartek.durys@gmail.com>
2021-05-15 19:09:00 +0200

    Shorten Is methods

--- Validator.py
+++ Validator.py
@@ -11,7 +11,7 @@ class Validator:
         pass
 
     def IsValidString(self,text,max_length) -> bool:
-        return (type(text)==str and 0<len(text)<=max_length)
+        return type(text)==str and 0<len(text)<=max_length
 
     def IsValidInt(self,value,max_val) -> bool:
         return type(value)==int and 0<value<=max_val
