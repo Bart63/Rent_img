9111e346c4d0612e22e3192d8303649e1dbb6b11
Bart63 <bartek.durys@gmail.com>
2021-05-18 01:10:24 +0200

    Proto of Client popup items

--- Client.py
+++ Client.py
@@ -1,10 +1,31 @@
+from configparser import NoOptionError
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 from GUI import GUI
 
 class Client_Screen(GridLayout):
-    rented = ObjectProperty(None)
-    available = ObjectProperty(None)
+    def Show_Items(self):
+        Items_Popup().open()
+
+class Items_Popup(Popup):
+    def __init__(self, **kwargs):
+        kwargs['title'] = 'Items'
+        super().__init__(**kwargs)
+
+    def on_open(self):
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+        layout.bind(minimum_height=layout.setter('height'))
+        for i in range(10):
+            btn = Button(text=str(i), size_hint_y=None, height=90, on_press=self.dismiss)
+            layout.add_widget(btn)
+        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        root.add_widget(layout)
+        self.add_widget(root)
+        return super().on_open()
 
 class Client(GUI):
     __balance__ = 0
@@ -14,21 +35,6 @@ class Client(GUI):
 
     def build(self):
         return Client_Screen()
-
-    def SetBalance(self):
-        pass
-
-    def Search(self):
-        pass
-
-    def Lend(self):
-        pass
-
-    def Show(self):
-        pass
-
-    def Open(self):
-        pass
 
 if __name__=='__main__':
     Client().run()
