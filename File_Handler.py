d4633dd75a30821878a2625de1383127a3f3b437
Bart63 <bartek.durys@gmail.com>
2021-05-23 20:01:39 +0200

    Code review changes

--- File_Handler.py
+++ File_Handler.py
@@ -1,35 +1,31 @@
-import os
+from os import remove
+from linecache import getline
+
 class File_Handler:
-    def Exist(self) -> bool:
-        pass
-
-    def Read(self,file ,n = -1):
-        f = open(file,'r')
-        if n >= 0 and n >= f:
-            for line in f:
-                f.readline()
-                if line == n:
-                    return f.readline()
+    def Read(self, filename, n=-1):
+        res = ""
+        if n==-1:
+            f = open(filename, 'r')
+            res = f.readlines()
+            f.close()
         else:
-            file_lines = [line.strip("\n") for line in f if line != "\n"]
-            return file_lines
-        f.close()
+            res = getline(filename, n)
+        return res
 
-    def Update(self,file,st,n = -1):
-        f = open(file,'w')
-        if n >= 0 and n >= f:
-            for line in f :
+    def Update(self, filename, st ,n=-1):
+        f = open(filename, 'w')
+        if n>=0 and n<sum(1 for _ in f):
+            for line in f:
                 f.readline()
                 if line == n:
                     f.write(st)
 
-
-    def Write(self, st,file):
-        f = open(file,'a')
-        f.write(file,st)
+    def Write(self, st, filename):
+        f = open(filename, 'a')
+        f.write(st+'\n')
         f.close()
 
-    def Clear(self,file):
-        os.remove(file)
-        f = open(file,'x')
+    def Clear(self, filename):
+        remove(filename)
+        f = open(filename, 'x')
         f.close()
--- Validator.py
+++ Validator.py
@@ -3,30 +3,16 @@ import os.path
 extentions = ['.png', '.jpg', '.jpeg']
 
 class Validator:
-    def IsString(self):
-        pass
+    def IsValidString(self, text, max_length) -> bool:
+        return type(text)==str and len(text)<=max_length and len(text)>0
 
-    def IsQuery(self) -> bool:
-        pass
+    def IsValidInt(self, value, max_val) -> bool:
+        return type(value)==int and value<=max_val and value>0
 
-    def IsBalance(self) -> bool:
-        pass
-
-    def IsValidString(self,text,max_length) -> bool:
-        if (type(text)==str and len(text)<=max_length and len(text)>0):
-            return True
-        else:
-            return False
-
-    def IsValidInt(self,value,max_val) -> bool:
-        if(type(value)==int and value<=max_val and value>0):
-            return True
-        else:
-            return False
-
-    def IsValidPath(self,path):
+    def IsValidPath(self, path) -> bool:
         if os.path.isfile(path):
             name,extention = os.path.splitext(path)
-            for ext in extentions :
-                if extention == extentions[ext]:
+            for ext in extentions:
+                if extention == ext:
                     return True
+        return False
